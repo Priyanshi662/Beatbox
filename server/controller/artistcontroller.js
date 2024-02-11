d66b47b89d272b5d1be73ad7c943923eeff16c07
Priyanshi662 <priyanshiw567@gmail.com>
2024-02-11 18:10:29 +0530

    topartist and getartist function added

--- server/controller/artistcontroller.js
+++ server/controller/artistcontroller.js
@@ -10,6 +10,28 @@ const getArtists = async (req, res) => {
 
 	res.status(200).json(artists);
 };
+const getTopArtists=async (req,res)=>{
+	const artists=await Artist.find();
+	if(!artists)
+		res.status(400).json({message:"No artist present"});
+	const topartists=artists.slice(1,11);
+	res.status(200).json(topartists);
+}
+const getArtistandSongs= async (req,res)=>{
+	const {id}=req.params;
+	const artist = await Artist.findById(id);
+	if(!artist)
+	{
+		res.status(400).json({message:"Artist does not exist!"});
+	}
+	// find if this artist is present in song schema or not
+	const songexist= await Song.find({artistIds:id});
+	if(!songexist)
+	{
+		res.status(400).json({message:"Some error occured"});
+	}
+	res.status(200).json({...artist._doc,songs:songexist});
+}
 export{
 	getArtists
 }
